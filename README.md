65e071e7f2482107c1f161ac7b05bf39f201cf6f
Irving Canales <irving@MacBook-Pro-de-Irving.local>
2020-07-07 14:08:41 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Hangman Game
+
+## Hangman game
+Just a hangman in vue js
---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <Hangman/>
+  </div>
+</template>
+
+<script>
+import Hangman from './components/Hangman'
+
+export default {
+  name: 'App',
+  components: {
+    Hangman
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: white;  
+  background-color: #2c3e50;
+}
+</style>
